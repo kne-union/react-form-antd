681cef1056a0c37632b101bf70249d21ea00455a
wen1 <571047994@qq.com>
2020-07-17 14:53:33 +0800

    添加头像剪切功能

--- src/fields/Avatar.js
+++ src/fields/Avatar.js
@@ -1,79 +1,144 @@
-import React, {useState} from 'react';
-import {Upload, message} from 'antd';
+import React, {useState, useRef, useEffect} from 'react';
+import {Upload, message, Modal} from 'antd';
 import {useOnChange} from '../hooks/useDecorator';
 import {globalParams} from '../preset';
 import classnames from 'classnames';
 import {
-  PlusOutlined,
-  LoadingOutlined
+    PlusOutlined,
+    LoadingOutlined
 } from '@ant-design/icons';
+import AvatarEditor from "react-avatar-editor";
 
 const avatarParams = globalParams.field.avatar;
 
 
-const _Avatar = ({className, value: imageUrl, onChange: propsChange, beforeUpload: onBeforeUpload, transformResponse, action, imageType, size, children, onError}) => {
-  console.log('\n\n\n/n/n/n',imageUrl);
-  const [loading, setLoading] = useState(false);
-  const uploadButton = (
-    <div>
-      {loading ? <LoadingOutlined/> : <PlusOutlined/>}
-      {children}
-    </div>
-  );
+const _Avatar = ({className, value: imageUrl, onChange: propsChange, beforeUpload: onBeforeUpload, transformResponse, action, imageType, size, children, onError, editor}) => {
+    const [loading, setLoading] = useState(false);
+    const [showImageUrl, setImageUrl] = useState(imageUrl);
+    const editorRef = useRef();
 
-  const beforeUpload = (file) => {
-    console.log('文件', file);
-    const isJpgOrPng = imageType.indexOf(file.type) > -1;
-    if (!isJpgOrPng) {
-      onError(`只支持${imageType.map((str) => str.replace('image/', '')).join('/')}格式图片!`, 'typeError', {
-        type: imageType,
-        fileType: file.type
-      });
-      return false;
-    }
-    const isLt = file.size / 1024 / 1024 < size;
-    if (!isLt) {
-      onError(`图片不能超过${size}MB!`, 'sizeError', {size, fileSize: file.size});
-      return false;
-    }
-    return onBeforeUpload && onBeforeUpload(file);
-  };
+    useEffect(() => {
+        setImageUrl(imageUrl)
+    }, [imageUrl])
 
-  const onChange = (info) => {
-    if (info.file.status === 'uploading') {
-      setLoading(true);
-      return;
-    }
-    if (info.file.status === 'done') {
-      setLoading(false);
-      const response = (transformResponse || avatarParams.transformResponse)(info.file.response);
-      if (response.code === 200) {
-        propsChange(response.results);
-      } else {
-        onError(response.msg, 'xhrError', response);
-      }
-    }
-  };
+    /* base64转blob */
+    const dataURLtoBlob = (dataurl) => {
+        let arr = dataurl.split(',');
+        // 注意base64的最后面中括号和引号是不转译的
+        let _arr = arr[1].substring(0, arr[1].length - 2);
+        let mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(_arr),
+            n = bstr.length,
+            u8arr = new Uint8Array(n);
+        while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new Blob([u8arr], {
+            type: mime
+        });
+    };
 
-  return (
-    <Upload className={classnames(className, 'react-form-avatar')} listType="picture-card" showUploadList={false}
-            action={action || avatarParams.action}
-            beforeUpload={beforeUpload} onChange={onChange}>
-      {imageUrl ? <div className="preview"><img src={imageUrl} alt="avatar"/></div> : uploadButton}
-    </Upload>
-  );
+    const uploadButton = (
+        <div>
+            {loading ? <LoadingOutlined/> : <PlusOutlined/>}
+            {children}
+        </div>
+    );
+
+    const beforeUpload = async (file) => {
+        return new Promise((resolve, reject) => {
+
+            console.log('文件', file);
+            const isJpgOrPng = imageType.indexOf(file.type) > -1;
+            if (!isJpgOrPng) {
+                onError(`只支持${imageType.map((str) => str.replace('image/', '')).join('/')}格式图片!`, 'typeError', {
+                    type: imageType,
+                    fileType: file.type
+                });
+                reject();
+                return false;
+            }
+            const isLt = file.size / 1024 / 1024 < size;
+            if (!isLt) {
+                onError(`图片不能超过${size}MB!`, 'sizeError', {size, fileSize: file.size});
+                reject();
+                return false;
+            }
+
+            /*剪切图像*/
+            // editor:{width:250,height:250,borderRadius:0-100,text:'剪切'}
+            if (editor) {
+                Modal.success({
+                    // title: '剪切图像',
+                    icon: '',
+                    content: <AvatarEditor
+                        ref={editorRef}
+                        image={file}
+                        width={editor.width || 250}
+                        height={editor.height || 250}
+                        border={50}
+                        borderRadius={editor.borderRadius || 1}
+                        color={[255, 255, 255, 0.9]} // RGBA
+                        scale={1.2}
+                        rotate={0}
+                    />,
+                    okText: editor.text || '剪切',
+                    onOk: (close) => {
+                        console.log(editorRef.current);
+                        let base64 = editorRef.current.getImage().toDataURL();
+                        setImageUrl(base64);
+                        let blob = dataURLtoBlob(base64);
+                        // console.log(blob);
+                        close();
+                        resolve(onBeforeUpload && onBeforeUpload(blob))
+                    }
+                })
+            } else {
+                return onBeforeUpload && onBeforeUpload(file);
+            }
+        })
+
+    };
+
+    const onChange = (info) => {
+        if (info.file.status === 'uploading') {
+            setLoading(true);
+            return;
+        }
+        if (info.file.status === 'done') {
+            setLoading(false);
+            const response = (transformResponse || avatarParams.transformResponse)(info.file.response);
+            if (response.code === 200) {
+                propsChange(response.results);
+            } else {
+                onError(response.msg, 'xhrError', response);
+            }
+        }
+    };
+
+    return (
+        <>
+            <Upload className={classnames(className, 'react-form-avatar')} listType="picture-card"
+                    showUploadList={false}
+                    action={action || avatarParams.action}
+                    beforeUpload={beforeUpload} onChange={onChange}>
+                {showImageUrl ? <div className="preview"><img src={showImageUrl} alt="avatar"/></div> : uploadButton}
+            </Upload>
+        </>
+    );
 };
 
 _Avatar.defaultProps = {
-  imageType: ['image/jpeg', 'image/png'],
-  size: 2,//单位MB
-  children: <div className="ant-upload-text">点击上传</div>,
-  onError: (info) => message.error(info)
+    imageType: ['image/jpeg', 'image/png'],
+    size: 2, // 单位MB
+    children: <div className="ant-upload-text">点击上传</div>,
+    onError: (info) => message.error(info),
+    editor: false
 };
 
 const AvatarInput = (props) => {
-  const render = useOnChange(props);
-  return render(_Avatar);
+    const render = useOnChange(props);
+    return render(_Avatar);
 };
 
 AvatarInput.field = _Avatar;
