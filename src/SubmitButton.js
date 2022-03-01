5099df8b3c9cab48f14e39a73b6ba007d7582070
zhipenglin <sunandmoon001@163.com>
2022-03-01 15:08:38 +0800

    修改构建脚本

--- src/SubmitButton.js
+++ src/SubmitButton.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {useSubmit} from '@kne/react-form';
 import {Button} from 'antd';
 
-const SubmitButton = (props) => {
+const SubmitButton = ({realTime, disabled, ...props}) => {
     const {isPass, isLoading, ...submitProps} = useSubmit(props);
-    return (<Button loading={isLoading} {...submitProps} {...props}/>);
+    return (
+        <Button loading={isLoading} disabled={disabled || (realTime ? !isPass : false)} {...submitProps} {...props}/>);
 };
 
 SubmitButton.defaultProps = {
-    type: 'primary'
+    type: 'primary', disabled: false
 };
 
 export default SubmitButton;
