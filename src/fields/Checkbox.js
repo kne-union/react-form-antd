4fd47b6b76c37073f823b0ac607c190c7b40dcc1
zhipenglin <sunandmoon001@163.com>
2023-04-10 19:00:20 +0800

    修改antd的checkbox修改bug

--- src/fields/Checkbox.js
+++ src/fields/Checkbox.js
@@ -7,7 +7,11 @@ const {withChecked} = hoc;
 const WithCheckbox = withChecked(_Checkbox);
 
 const Checkbox = (props) => {
-    const checkedProps = useCheckedToValue(props);
+    const checkedProps = useCheckedToValue(Object.assign({}, props, {
+        onChange: (e) => {
+            props.onChange && props.onChange(e.target.checked);
+        }
+    }));
     const render = useOnChange(checkedProps);
     return render(WithCheckbox);
 };
