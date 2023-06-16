d472c8e3ef16aa17c296f8af8b74c469c884f13b
zhipenglin <sunandmoon001@163.com>
2023-06-16 16:15:45 +0800

    修改导出问题

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ import Upload$ from './fields/Upload';
 
 export * from './Form';
 export default Form;
+export {Form as FormAntd};
 export {default as preset} from './preset';
 export {default as FormModal} from './FormModal';
 export {default as ResetButton} from './ResetButton';
