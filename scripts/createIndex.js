6473f4e92f6bf24b006c2d62bb8e83e4964aed31
zhipenglin <sunandmoon001@163.com>
2023-07-24 15:24:58 +0800

    删除不兼容组件

--- scripts/createIndex.js
+++ scripts/createIndex.js
@@ -1,27 +1,24 @@
-const path = require('path');
-const glob = require('pify')(require('glob'));
-const fs =require('fs-extra');
+import path from 'path';
+import fs from 'fs-extra';
+import {glob} from "glob";
 
-glob(path.resolve(__dirname,'../src/fields/**/*.js')).then((files)=>{
+glob('src/fields/**/*.js').then((files)=>{
   const filenames = files.map((dir)=>{
     return path.basename(dir,path.extname(dir));
   });
   const fileContent = `import Form from './Form';
 ${filenames.map((name)=>`
-import ${name}$ from './fields/${name}';`).join('')}
+import ${name} from './fields/${name}';`).join('')}
 
 export * from './Form';
 export default Form;
 export {default as preset} from './preset';
-export {default as FormModal} from './FormModal';
 export {default as ResetButton} from './ResetButton';
 export {default as SubmitButton} from './SubmitButton';
 export {default as CancelButton} from './CancelButton';
-${filenames.map((name)=>`
-export const ${name} = ${name}$;`).join('')}
+export {${filenames.join(', ')}};
 
 export const fields = { ${filenames.join(', ')} };`
-
-  return fs.writeFile(path.resolve(__dirname,'../src/index.js'),fileContent);
+  return fs.writeFile(path.resolve('src/index.js'),fileContent);
 });
 
