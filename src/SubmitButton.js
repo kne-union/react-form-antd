38446f590fe3fc93c00f545ca48949dda2c3ca20
zhipenglin <sunandmoon001@163.com>
2024-06-14 15:15:42 +0800

    修改SubmitButton realtime参数不规范问题

--- src/SubmitButton.js
+++ src/SubmitButton.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {useSubmit} from '@kne/react-form';
 import {Button} from 'antd';
 
-const SubmitButton = ({realTime, disabled, ...props}) => {
+const SubmitButton = ({realTime, realtime, disabled, ...props}) => {
     const {isPass, isLoading, ...submitProps} = useSubmit(props);
-    return (
-        <Button loading={isLoading} disabled={disabled || (realTime ? !isPass : false)} {...props} {...submitProps}/>);
+    if (typeof realTime === 'boolean') {
+        console.error(`warning: realTime参数已废弃，请使用realtime，后续版本可能回删除realTime的支持`);
+    }
+    // 如果传入 realtime 优先使用该属性
+    return (<Button loading={isLoading}
+                    disabled={disabled || ((typeof realtime === 'boolean' ? realtime : realTime) ? !isPass : false)} {...props} {...submitProps}/>);
 };
 
 SubmitButton.defaultProps = {
